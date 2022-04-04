6f597281c38156c45d6e3ce3e037ae8e9cc51206
RahulBajaj23 <rahulbajaj.rb23@gmail.com>
2022-04-04 12:27:23 +0530

    Fix error

---
+++ OddEven.py
@@ -0,0 +1,16 @@
+list1 = []
+countEven = 0
+countOdd = 0
+num = int(input("Enter No. of Elements : "))
+for i in range(num):
+    list1.append(int(input("Enter Ele : ")))
+    
+for i in list1:
+    if i % 2 == 0:
+        countEven += 1
+    elif i % 2 != 0:
+        countOdd += 1
+    else:
+        print("Invalid No.")
+
+print("Odd Count : {}, Even Count : {}".format(countOdd, countEven))
